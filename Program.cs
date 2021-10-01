3ab6b9f216cd44b544a1fb32fed84d5082548611
Francesco Meloni <FRANCESCO.MELONI@local.ittcesena.it>
2021-10-01 09:31:53 +0200

    v1.1 logica e output numero minimo

--- Program.cs
+++ Program.cs
@@ -6,7 +6,20 @@ namespace MinMax
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            int[] array = { 3, 6, -3, 5, 7 };
+            int min = array[0];
+
+            for (int i = 1; i < array.Length; i++)
+            {
+                if (min> array[i])
+                {
+                    min = array[i];
+                }
+            }
+
+            Console.WriteLine("il valore minimo è: " + min);
+            
+            Console.ReadKey();
         }
     }
 }
